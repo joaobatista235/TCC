476c453a00acff483accc533f10e230467d33565
joaobatista235 <62124351+joaobatista235@users.noreply.github.com>
2021-02-19 13:49:48 -0300

    Add files via upload

---
+++ images/busca.php
@@ -0,0 +1,28 @@
+<?php
+/*
+include ('conexao/connect.php');
+
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM pessoas WHERE PesId = '$id'";
+    $registros = $conn -> query($sql);*/
+    echo "hi"
+;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="circle" style="margin-left:20px;">
+                      <img class="circle-img" src="images/joao.jpg">
+                    </div>
+                    <div class="col-6">
+                      <p>Bom dia, eu gostaria de saber o frete de entrega do produto x para entrega na cidade y.</p>
+                    </div>
+</body>
+</html>
